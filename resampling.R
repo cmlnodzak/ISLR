b41085f3c66ef1f68008fb81e3b9b23f96fffb91
cmlnodzak <cmlnodzak@users.noreply.github.com>
2018-11-30 12:12:16 -0500

    added K-fold CV

--- resampling.R
+++ resampling.R
@@ -25,6 +25,17 @@ plot(degree,cv.error,type="b")
 
 ### 10-fold CV
 
+cv.error10<-rep(0,5)
+for(d in degree){
+	glm.fit<-glm(mpg~poly(horsepower,d),data=Auto)
+	cv.error10[d]<-cv.glm(Auto,glm.fit,K=10)$delta[1]
+}
+lines(degree,cv.error10,type="b",col="red")
+
+
+
+
+
 
 
 
