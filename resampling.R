f58d978e73e6ac41fc46a2988675f200b7d99d72
cmlnodzak <cmlnodzak@users.noreply.github.com>
2018-11-30 12:08:52 -0500

    added LOOCV function

--- resampling.R
+++ resampling.R
@@ -4,7 +4,28 @@ require(boot)
 plot(mpg~horsepower,data=Auto)
 
 ### Leave one out cross validation (LOOCV)
-glm.fit<-glm
+glm.fit<-glm(map~horsepower,data=Auto)
+cv.glm(Auto,glm.fit$delta) # slow, does not use formula 5.2
+
+### function to make use of LOOCV formula to speed up cv
+
+loocv<-function(fit){
+	h=lm.influence(fit)$h
+	mean((residuals(fit)/(1-h))^2)
+}
+
+loocv(glm.fit)
+cv.error<-rep(0,5)
+degree<-1:5
+for(d in degree){
+	glm.fit<-glm(mpg~poly(horsepower,d),data=Auto)
+	cv.error[d]<-loocv(glm.fit)
+}
+plot(degree,cv.error,type="b")
+
+### 10-fold CV
+
+
 
 
 
