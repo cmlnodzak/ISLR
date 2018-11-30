a676b373858f2ec335c284573cab27642ed3ec8a
cmlnodzak <cmlnodzak@users.noreply.github.com>
2018-11-30 14:30:32 -0500

    added leaps library

--- ModelSelection.R
+++ ModelSelection.R
@@ -1,1 +1,30 @@
-#placeholder for model selection...
+library(ISLR)
+summary(Hitters)
+
+### remove missing values
+Hitters<-na.omit(Hitters)
+with(Hitters,sum(is.na(Salary)))
+
+
+### Best subset regression model selection with leaps library.
+library(leaps)
+regfit.full<-regsubsets(Salary~., data=Hitters)
+summary(regfit.full)
+
+
+regfit.full<-regsubsets(Salary~.,data=Hitters,nvmax=19)
+reg.summary<-summary(regfit.full)
+names(reg.summary)
+plot(reg.summary$cp,xlab="Number of Variables",ylab="Cp")
+which.min(reg.summary$cp)
+
+points(10,reg.summary$cp[10],pch=20,col="red")
+
+### regsubsets objects have plot method defined
+
+plot(regfit.full,scale="Cp")
+coef(regfit.full,10)
+
+
+
+
