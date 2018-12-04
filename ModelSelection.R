cd83da792fb872b4f3e3974eb97924ddd60aad69
cmlnodzak <cmlnodzak@users.noreply.github.com>
2018-12-04 13:43:21 -0500

    added cross validation

--- ModelSelection.R
+++ ModelSelection.R
@@ -61,4 +61,27 @@ predict.regsubsets<-function(object,newdata,i,...){
 	mat[,names(coef)]%*%coefi
 }
 
+### The training error goes down monotonically as the model gets bigger, but not so for the validation error.
+
+
+### Model Selection by Cross Validation
+set.seed(11)
+folds=sample(rep(1:10,length=nrow(Hitters)))
+table(folds)
+cv.errors=matrix(NA,10,19)
+for(k in 1:10){
+	best.fit=regsubsets(Salary~.,data=Hitters[folds!=k,],nvmax=19,method="forwards")
+	for(i in 1:19){
+	pred=predict(best.fit,Hitters[folds==k,],id=i)
+	cv.errors[k,i]=mean((Hitters$Salary[folds==k]-pred)^2)
+	}
+}
+
+rmse.cv=sqrt(apply(cv.errors,2,mean))
+plot(rmse.cv,pch=19,type="b")
+
+
+
+
+
 
