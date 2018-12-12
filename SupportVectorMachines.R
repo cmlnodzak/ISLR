9c92b5a3de1d29eb930e480e6e99920943033a01
cmlnodzak <cmlnodzak@users.noreply.github.com>
2018-12-12 12:56:08 -0500

    implement expand.grid function

--- SupportVectorMachines.R
+++ SupportVectorMachines.R
@@ -18,6 +18,18 @@ print(svmfit)
 plot(svmfit,dat)
 
 
+### The plot function here is crude, and plots X2 on the horizontal axis (unlike what R normally does for a matrix). 
+### To make our own plot, we need to make a function to provide a grid of values for X1 and X2.
+### We use the function expand.grid, and produce the coordinates of n*n points on a lattice covering the domain of "x".
+### Having made the lattice, we then make a prediction at each point of the lattice.
+### We then plot the lattice, color-coded according to the classification and visualize the decision boundary.
 
+### The support points, (i.e. points on the margin, or on the wrong side of the margin) are indexed in the "$index" component of the fit.
 
+make.grid<-function(x,n=75){
+	grange<-apply(x,2,range)
+	x1<-seq(from=grange[1,1],to=grange[2,1],length=n)
+	x2<-seq(from=grange[1,2],to=grange[2,2],length=n)
+	expand.grid(X1=x1,X2=x2)
+}
 
